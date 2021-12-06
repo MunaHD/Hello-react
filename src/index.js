5c748f026e1dafb5a4f70ce8462297417288342f
Muna Dahir <muna.dahir@hotmail.com>
2021-12-06 16:43:03 -0500

    finish the hello-react app

--- src/index.js
+++ src/index.js
@@ -3,22 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
-  // your code here
+  return (
+    <button onClick={props.reset}>
+      {props.text}
+    </button>
+  );
 };
 
 const Application = () => {
-
-  // your code here
-
+  const[name, setName] = useState("")
+  const nameLength = name.length
   const reset = () => {
-    console.log("reset");
-    // your code here
+    setName("")
   };
 
   return (
     <main>
       {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input
+      value={name}
+      onChange={(event) => setName(event.target.value)}
+      placeholder="Type your name" 
+      />
+      <Button text="reset" reset={reset} />
+      { nameLength  ? <h1>Hello {name}</h1> : ''}
+      
     </main>
   );
 };
